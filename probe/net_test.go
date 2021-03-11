97abea39b471f7890c940a9e1baec4c285cc82f4
Frank Ratschinski <fratschin@gmail.com>
2021-03-11 10:44:19 +0100

    DNS probe added

---
+++ probe/net_test.go
@@ -0,0 +1,36 @@
+package probe
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/url"
+	"testing"
+)
+
+func TestNewDnsProbeFail(t *testing.T) {
+	_,ok := NewDnsProbe("none")()
+	assert.False(t, ok)
+}
+
+func TestNewHttpGetProbeFail(t *testing.T) {
+	u, _ := url.Parse("httpX://httpbin.org/get")
+	_, ok := NewHttpGetProbe(*u)()
+	assert.False(t, ok)
+}
+
+func TestNewHttpGetProbe(t *testing.T) {
+	u, _ := url.Parse("https://httpbin.org/get")
+	_, ok := NewHttpGetProbe(*u)()
+	assert.True(t, ok)
+}
+
+func TestNewHttpGetProbeStatus(t *testing.T) {
+	u, _ := url.Parse("https://httpbin.org/get")
+	_, ok := NewHttpGetProbeStatus(*u, 200)()
+	assert.True(t, ok)
+}
+
+func TestNewHttpGetProbeStatus400(t *testing.T) {
+	u, _ := url.Parse("https://httpbin.org/status/400")
+	_, ok := NewHttpGetProbeStatus(*u, 200)()
+	assert.False(t, ok)
+}
